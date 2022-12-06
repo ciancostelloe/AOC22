4a742669e350fde5972cd3842de785ba2e7ff29f
Cian Costelloe <ciancostelloe@Cians-MacBook-Air-2.local>
2022-12-06 14:25:31 -0800

    Part 2

--- Day1.py
+++ Day1.py
@@ -8,11 +8,13 @@ for line in lines:
     if line != '\n':
         list_of_calories_per_elf.append(int(line))
     else:
-        list_of_calories_per_elf = []
-    if list_of_calories_per_elf:
         total = sum(list_of_calories_per_elf)
         total_list.append(total)
+        list_of_calories_per_elf = []
 answer = max(total_list)
 print(answer)
-    
-    
+
+'''Part 2'''
+sorted_list = sorted(total_list)
+num_of_elves = len(total_list)
+print(sorted_list[num_of_elves - 1] + sorted_list[num_of_elves - 2] + sorted_list[num_of_elves - 3])
