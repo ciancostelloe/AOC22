f8d0b14420fa385c838b1eb1a245c656520d3258
Cian Costelloe <ciancostelloe@Cians-MacBook-Air-2.local>
2022-12-06 12:53:35 -0800

    Day1 part 1

--- Day1.py
+++ Day1.py
@@ -1,2 +1,19 @@
 '''AOC 2022'''
 '''Day 1.1'''
+with open('Day1_input.txt') as f:
+    lines = f.readlines()
+
+list_of_calories_per_elf = []
+total_list = []
+for line in lines:
+    if line != '\n':
+        list_of_calories_per_elf.append(int(line))
+    else:
+        list_of_calories_per_elf = []
+    if list_of_calories_per_elf:
+        total = sum(list_of_calories_per_elf)
+        total_list.append(total)
+answer = max(total_list)
+print(answer)
+    
+    
